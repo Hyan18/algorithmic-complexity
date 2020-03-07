3451f4eb0f9f7a565bd60e2166c46450b8847e34
Hyan18 <HarrisonYan18@gmail.com>
2020-03-07 17:48:30 +0000

    fisher-yates shuffle

--- shuffle.rb
+++ shuffle.rb
@@ -4,7 +4,7 @@ a = 5000
 b = 100000
 count = b / a
 
-def my_shuffle(array)
+def bad_shuffle(array)
   arr = array.dup
   new_arr = []
   until arr.empty?
@@ -12,6 +12,17 @@ def my_shuffle(array)
     new_arr << arr.delete_at(i)
   end
   return new_arr
+end
+
+def fy_shuffle(array)
+  arr = array.dup
+  length = arr.length
+  while length > 1
+    r = rand(length)
+    arr[r], arr[length-1] = arr[length-1], arr[r]
+    length -= 1
+  end
+  arr
 end
 
 for i in 1..count do
@@ -22,7 +33,7 @@ for i in 1..count do
   times = 100
 
   for i in 1..times do
-    results << Benchmark.realtime { my_shuffle(arr) }
+    results << Benchmark.realtime { fy_shuffle(arr) }
   end
   out = times*0.05
 
