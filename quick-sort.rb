94d10ba2781af949fe814998194de05a129dbb2a
Hyan18 <HarrisonYan18@gmail.com>
2020-03-15 17:45:45 +0000

    quicksort random pivot

--- quick-sort.rb
+++ quick-sort.rb
@@ -1,9 +1,32 @@
-def quick_sort(arr)
+require 'benchmark'
+
+a = 50000
+b = 100000
+count = b / a
+
+def first_quick_sort(arr)
   return arr if arr.length < 2
-  pivot = arr[0]
+  r = 0
+  pivot = arr[r]
   lower = []
   upper = []
-  arr[1..].each { |n|
+  (arr[...r] + arr[(r+1)..]).each { |n|
+    if n < pivot
+      lower.append(n)
+    else
+      upper.append(n)
+    end
+  }
+  first_quick_sort(lower) + [pivot] + first_quick_sort(upper)
+end
+
+def quick_sort(arr)
+  return arr if arr.length < 2
+  r = rand(arr.length)
+  pivot = arr[r]
+  lower = []
+  upper = []
+  (arr[...r] + arr[(r+1)..]).each { |n|
     if n < pivot
       lower.append(n)
     else
@@ -12,3 +35,24 @@ def quick_sort(arr)
   }
   quick_sort(lower) + [pivot] + quick_sort(upper)
 end
+
+
+for i in 1..count do
+  size = a*i
+  arr = ((1..size).to_a).shuffle
+  results = []
+
+  times = 20
+
+  for i in 1..times do
+    results << Benchmark.realtime { quick_sort(arr) }
+  end
+  out = times*0.05
+
+  results.sort!
+  results.shift(out)
+  results.pop(out)
+  time = (results.inject(:+)/results.length) * (10**9)
+  puts "#{size} #{time}"
+end
+
