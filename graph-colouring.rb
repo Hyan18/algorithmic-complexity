d7aa674dfaad0af4b5862c5036a6ab0285892e01
Hyan18 <HarrisonYan18@gmail.com>
2020-03-19 19:22:59 +0000

    bigger graph colouring

--- graph-colouring.rb
+++ graph-colouring.rb
@@ -13,16 +13,70 @@ class GraphNode
   end
 end
 
+# Small Graph
+
+# a = GraphNode.new("a")
+# b = GraphNode.new("b")
+# c = GraphNode.new("c")
+
+# a.neighbors << b
+# b.neighbors << a
+# b.neighbors << c
+# c.neighbors << b
+
+# graph = [a, b, c]
+
 a = GraphNode.new("a")
 b = GraphNode.new("b")
 c = GraphNode.new("c")
+d = GraphNode.new("d")
+e = GraphNode.new("e")
+f = GraphNode.new("f")
+g = GraphNode.new("g")
+h = GraphNode.new("h")
+i = GraphNode.new("i")
+j = GraphNode.new("j")
+k = GraphNode.new("k")
+l = GraphNode.new("l")
 
 a.neighbors << b
+a.neighbors << c
+a.neighbors << e
 b.neighbors << a
-b.neighbors << c
-c.neighbors << b
+b.neighbors << d
+b.neighbors << g
+c.neighbors << a
+c.neighbors << e
+c.neighbors << f
+d.neighbors << b
+d.neighbors << f
+d.neighbors << g
+e.neighbors << a
+e.neighbors << c
+e.neighbors << h
+f.neighbors << c
+f.neighbors << d
+f.neighbors << j
+g.neighbors << b
+g.neighbors << d
+g.neighbors << l
+h.neighbors << e
+h.neighbors << i
+h.neighbors << k
+i.neighbors << h
+i.neighbors << j
+i.neighbors << k
+j.neighbors << f
+j.neighbors << i
+j.neighbors << l
+k.neighbors << h
+k.neighbors << i
+k.neighbors << l
+l.neighbors << g
+l.neighbors << j
+l.neighbors << k
 
-graph = [a, b, c]
+graph = [a, b, c, d, e, f, g, h, i, j, k, l]
 
 def colour(graph)
   raise "Graph cannot have any loops" if has_loop?(graph)
