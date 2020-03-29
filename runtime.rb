52bb84eca9d502ed9c436c22c971fd6e093f6e11
Hyan18 <HarrisonYan18@gmail.com>
2020-03-29 12:31:58 +0100

    refactor out runtime function

---
+++ runtime.rb
@@ -0,0 +1,23 @@
+require 'benchmark'
+
+def runtime(algorithm, a = 5000, b = 100000)
+  count = b / a
+  for i in 1..count do
+    size = a*i
+    arr = ((1..size).to_a).shuffle
+    results = []
+
+    times = 100
+
+    for i in 1..times do
+      results << Benchmark.realtime { algorithm.run(arr) }
+    end
+    out = times*0.05
+
+    results.sort!
+    results.shift(out)
+    results.pop(out)
+    time = (results.inject(:+)/results.length) * (10**9)
+    puts "#{size} #{time}"
+  end
+end
