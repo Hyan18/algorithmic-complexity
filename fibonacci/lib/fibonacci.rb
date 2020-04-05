2286c6dc24c995b994b125bb5b39d9c0162d905d
Hyan18 <HarrisonYan18@gmail.com>
2020-04-05 20:39:10 +0100

    fibonacci analysis

--- fibonacci/lib/fibonacci.rb
+++ fibonacci/lib/fibonacci.rb
@@ -13,3 +13,23 @@ module Fibonacci
   end
 
 end
+
+require 'benchmark'
+
+for i in 1..100 do
+  results = []
+  size = i*100
+
+  times = 100
+
+  for j in 1..times do
+    results << Benchmark.realtime { Fibonacci.values(size) }
+  end
+  out = times*0.05
+
+  results.sort!
+  results.shift(out)
+  results.pop(out)
+  time = (results.inject(:+)/results.length) * (10**9)
+  puts "#{size} #{time}"
+end
