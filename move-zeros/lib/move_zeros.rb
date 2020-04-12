f3b93967277e4e2e7d163811c29ca4732602ccc8
Hyan18 <HarrisonYan18@gmail.com>
2020-04-12 11:54:15 +0100

    alternate move zeros method

--- move-zeros/lib/move_zeros.rb
+++ move-zeros/lib/move_zeros.rb
@@ -13,4 +13,46 @@ module MoveZeros
       i += 1
     end
   end
+
+  # Alternative method
+
+  def self.alt(nums)
+    i = 0
+    nums.each do |n|
+        if n != 0
+            nums[i] = n
+            i += 1
+        end
+    end
+    (i...nums.size).each {|indx| nums[indx] = 0}
+    return nums
+  end
+
+end
+
+require 'benchmark'
+
+a = 50
+b = 1000
+
+count = b / a
+for i in 1..count do
+  size = a*i
+  x = rand(size)
+  arr = Array.new(x, 0) + (1..(size - x)).to_a
+  arr.shuffle!
+  results = []
+
+  times = 100
+
+  for i in 1..times do
+    results << Benchmark.realtime { MoveZeros.alt(arr) }
+  end
+  out = times*0.05
+
+  results.sort!
+  results.shift(out)
+  results.pop(out)
+  time = (results.inject(:+)/results.length) * (10**9)
+  puts "#{size} #{time}"
 end
